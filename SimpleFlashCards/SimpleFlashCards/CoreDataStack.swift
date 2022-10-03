8cf31cc002bfdc79d4b6078d9e4eb5d15e7daf99
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2022-10-03 16:56:52 -0400

    Add CoreDataStack

---
+++ SimpleFlashCards/SimpleFlashCards/CoreDataStack.swift
@@ -0,0 +1,26 @@
+//
+//  CoreDataStack.swift
+//  SimpleFlashCards
+//
+//  Created by Bradley GIlmore on 10/3/22.
+//
+
+import Foundation
+import CoreData
+
+enum CoreDataStack {
+    
+    static let container: NSPersistentContainer = {
+        
+        let appName = Bundle.main.object(forInfoDictionaryKey: (kCFBundleNameKey as String)) as! String
+        let container = NSPersistentContainer(name: appName)
+        container.loadPersistentStores() { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    static var context: NSManagedObjectContext { return container.viewContext }
+}
